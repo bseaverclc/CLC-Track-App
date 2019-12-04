88233554ddc33078d650237de4c030493e340762
Brian Seaver <bseaver@d155.org>
2019-12-03 21:51:03 -0600

    new view controller for register a new account creating an account and new account showing up in table view works!

--- CLC Track App/RegisterViewController.swift
+++ CLC Track App/RegisterViewController.swift
@@ -9,20 +9,52 @@
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+  @IBOutlet weak var emailOutlet: UITextField!
     @IBOutlet weak var passwordOutlet: UITextField!
-    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var firstOutlet: UITextField!
+    @IBOutlet weak var lastOutlet: UITextField!
+    @IBOutlet weak var yearOutlet: UITextField!
+    @IBOutlet weak var eventOutlet: UITextField!
+    @IBOutlet weak var imageViewOutlet: UIImageView!
     var ref : DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         ref = Database.database().reference()
         
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func takePicAction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+                   imagePicker.sourceType = UIImagePickerController.SourceType.camera
+               }
+               else{
+                   imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+               }
+               present(imagePicker, animated: true, completion: nil)
+           }
+           
+           func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+               picker.dismiss(animated: true){
+                   let pickedImage = info[.originalImage] as? UIImage
+                   self.imageViewOutlet.image = pickedImage
+                   //self.saveImageToFirebase(image: pickedImage!)
+               }
+    }
+    
+    
+    @IBAction func uploadPicAction(_ sender: UIButton) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func registerAction(_ sender: UIButton) {
         
         Auth.auth().createUser(withEmail: emailOutlet.text!, password: passwordOutlet.text!) { (result, error) in
@@ -31,9 +63,25 @@ class RegisterViewController: UIViewController {
             }
             else{
                 if let uid = result?.user.uid{
-                print(uid)
+                    print(uid)
+                   
+                    
+                    
                 }
             }
         }
+        
+        if let currentUser = Auth.auth().currentUser?.uid {
+            let ath = ["name": self.firstOutlet.text!,
+            "year": self.yearOutlet.text!
+             ]
+        ref.child(currentUser).setValue(ath)
+            
+            
+                                
+            
+                //adding the artist inside the generated unique key
+           // self.ref.child(key).setValue(ath)
     }
+}
 }
